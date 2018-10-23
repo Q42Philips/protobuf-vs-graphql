f000c0c6832f546e7e44ac9e52e297935c6c6db9
EMLGaming <EMLGaming@protonmail.com>
2018-10-23 09:03:20 +0000

    init

---
+++ json/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3333
+
+app.get('/', (req, res) => res.send(
+    {
+        "1":1234,
+        "2":"helloworldstring",
+        "3":Date(),
+        "4":"1234567890",
+        "5":true
+    }
+    ))
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
